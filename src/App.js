889095b1523192b4a517462c8e4b14e423c38841
Andre Maesha <andremaesha@gmail.com>
2021-08-26 18:15:40 +0700

    add multi upload file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
+import Resizer from "react-image-file-resizer";
+
+const resizeFile = (file) =>
+    new Promise((resolve) => {
+        Resizer.imageFileResizer(
+            file,
+            300,
+            400,
+            "JPEG",
+            80,
+            0,
+            (uri) => {
+                resolve(uri);
+            },
+            "base64"
+        );
+    });
+
 const App = () => {
+    // const [previewAvatar, setPreviewAvatar] = useState("");
     const [images, setImage] = useState([]);
 
     const onChange = (event) => {
+        // try {
+        //     const file = event.target.files[0];
+        //     const image = await resizeFile(file);
+        //     console.log(image);
+        //     setPreviewAvatar(image);
+        // } catch (error) {
+        //     console.log(error);
+        // }
         const files = Array.from(event.target.files);
 
         console.log(files);
